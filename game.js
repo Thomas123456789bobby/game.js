d9dd0e0ad16c53529664128d5f40cf06d7d5cf14
thomas <46562812+Thomas123456789bobby@users.noreply.github.com>
2021-10-14 12:52:52 +0200

    ww

---
+++ game.js
@@ -0,0 +1,24 @@
+//alert("ow nee");
+// dit is een alert
+//document.getElementById("ding_1").innerHTML = "hallo mensen";
+// een id doe hij van htmk kan hellen en dan html kan toe voegen
+
+//function changetext(){
+    
+//document.getElementById("ding_1").innerHTML = "doei mensen";
+//console.log("change text")}
+// dit is een function die de text verander als je er op kilt en als je op een een consel.log
+
+function allowDrop(ev) {
+    ev.preventDefault();
+  }
+  
+  function drag(ev) {
+    ev.dataTransfer.setData("text", ev.target.id);
+  }
+  
+  function drop(ev) {
+    ev.preventDefault();
+    var data = ev.dataTransfer.getData("text");
+    ev.target.appendChild(document.getElementById(data));
+  }
